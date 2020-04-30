2d23d37e9efeb83279e0106825ec560858b8e996
DangWang <megistios@gmail.com>
2020-04-30 13:14:33 +0300

    Automated the addition of entires in  the monster menu

---
+++ Assets/Scripts/scr_MonsterMenu.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class scr_MonsterMenu : MonoBehaviour
+{
+    void Start()
+    {
+        var parent = transform.ReturnRoot().gameObject;
+        var parentController = parent.GetComponent<scr_Player>();
+
+        var i = 0;
+        foreach (var monsterType in parentController.monsterTypes)
+        {
+            if (i < 18) // current limit
+            {
+                var monsterScript = monsterType.GetComponent<scr_Monster>();
+                var monsterSprite = monsterType.transform.Find("Token").GetComponent<SpriteRenderer>().sprite;
+                var menuElement = transform.Find("Monster_" + i.ToString()).gameObject;
+                menuElement.transform.Find("Image").GetComponent<Image>().sprite = monsterSprite;
+                menuElement.transform.Find("Text").GetComponent<Text>().text = "Cost: " + monsterScript.monsterCost;
+            }
+            else
+            {
+                break;
+            }
+            i++;
+        }
+    }
+}
